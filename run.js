4ce76658d24bf61fd72cde79b316628b56cba401
Li-Xian Chen <b0543017@ems.niu.edu.tw>
2019-03-24 18:23:41 +0800

    revert

--- run.js
+++ run.js
@@ -1,7 +1,5 @@
 require("./lib/ArrayTools");
 
-console.log("Ready to run.");
-
 var macaddress = require('macaddress');
 var macAddr = macaddress.networkInterfaces();
 
@@ -23,37 +21,31 @@ var pk2 = new Lamp("pk2");
 var blu = new Lamp("blu");
 var whi = new Lamp("whi");
 
-var sync = false;
-var startPlayer = new Player(__dirname + "/" + "./audio/blue_line.mp3");
-startPlayer.play();
-startPlayer.on("player_exit", function () {
-    console.log("DONE");
-    sync = true;
-});
-
 event.on('serial_port_ready', function () {
-    
-    while(!sync);
-
     pk1.scaleTo(0, 1000);
     pk2.scaleTo(0, 1000);
     blu.scaleTo(0, 1000);
     whi.scaleTo(0, 1000);
 
-    setTimeout(function () {
-        var networkSound = new Player(__dirname + "/" + "./audio/waiting_network.mp3");
-        networkSound.play();
-        networkSound.on("player_exit", function () {
-            connectivity(function (online) {
-                pk1.scaleTo(125, 1000);
-                if (online) {
-                    localEvent.emit('network_online');
-                } else {
-                    localEvent.emit('network_offline');
-                }
+    var startPlayer = new Player(__dirname + "/" + "./audio/blue_line.mp3");
+    startPlayer.play();
+    startPlayer.on("player_exit", function () {
+        setTimeout(function () {
+            var networkSound = new Player(__dirname + "/" + "./audio/waiting_network.mp3");
+            networkSound.play();
+            networkSound.on("player_exit", function () {
+                connectivity(function (online) {
+                    pk1.scaleTo(125, 1000);
+                    if (online) {
+                        localEvent.emit('network_online');
+                    } else {
+                        localEvent.emit('network_offline');
+                    }
+                });
             });
-        });
-    }, 1000);
+        }, 1000);
+
+    });
 
     localEvent.on("network_online", function () {
         pk1.scaleTo(0, 1000);
@@ -70,21 +62,16 @@ event.on('serial_port_ready', function () {
 
     localEvent.on("ready_to_iottalk", function () {
         dai.register();
-        blu.scaleTo(125, 1000);
     });
 
     dai.on("registered", function () {
         var connectedSound = new Player(__dirname + "/" + "./audio/connected.mp3");
         connectedSound.play();
         connectedSound.on("player_exit", function () {
-            blu.scaleTo(0, 1000);
-            whi.scaleTo(255, 1000, function () {
-                whi.scaleTo(0, 1000, function () {
-                    localEvent.emit("ready");
-                });
-            });
+            localEvent.emit("ready");
         });
     });
+
 });
 
 localEvent.on("ready", function () {
